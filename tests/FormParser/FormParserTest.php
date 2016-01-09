a0d4e24f188abe5fb408f2bc3f0fc3c9d0126754
Matthew Laver <matthew.laver@reasondigital.com>
2016-01-09 11:44:14 +0000

    initial commit

---
+++ tests/FormParser/FormParserTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mlaver
+ * Date: 09/01/2016
+ * Time: 11:38
+ */
+
+namespace FormParser;
+
+
+class FormParserTest extends \PHPUnit_Framework_TestCase {
+
+	public function test_form_parser_returns_true(){
+		$this->assertTrue(true);
+	}
+
+}
